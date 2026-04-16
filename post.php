e8886c05cda9948455ea491846e048694871835a
Patric <pmorevs@gmail.com>
2026-04-16 14:12:01 +0300

    16 april

--- post.php
+++ post.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['name'])){
 	$myText = $_POST['text'];
-	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span><b class='user-name'>".$_SESSION['name']."</b> ".$myText."<br></div>";
+	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".$myText."<br></div>";
 	file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
